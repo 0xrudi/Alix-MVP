51e138c2e9c28098ec1a084556c2c2193319ddc7
rudnickandrew <andrew@rudini.xyz>
2025-01-29 08:59:01 -0700

    Added missing build dependencies and polyfills

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,26 @@
 const webpack = require('webpack');
 
-module.exports = function override(config, env) {
+module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    crypto: require.resolve('crypto-browserify'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify/browser'),
-    url: require.resolve('url'),
-    process: false,
-    vm: require.resolve('vm-browserify')
+    "vm": require.resolve("vm-browserify"),
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert/"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify/browser"),
+    "url": require.resolve("url"),
+    "buffer": require.resolve("buffer/")
   };
 
-  config.plugins = (config.plugins || []).concat([
+  config.plugins = [
+    ...config.plugins,
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
     }),
-  ]);
+  ];
 
   return config;
-}
+};
