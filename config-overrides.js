a388877cce7c8a6ae2302e2994119e1ee7802540
rudnickandrew <andrew@rudini.xyz>
2025-01-30 16:38:03 -0700

    package updates

--- config-overrides.js
+++ config-overrides.js
@@ -1,29 +1,30 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  config.resolve = {
-    ...config.resolve,
-    fallback: {
-      ...config.resolve.fallback,
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "assert": require.resolve("assert/"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "os": require.resolve("os-browserify/browser"),
-      "url": require.resolve("url/"),
-      "vm": require.resolve("vm-browserify"),
-      "process": require.resolve("process/browser")
-    }
+  // Add polyfill fallbacks
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "process/browser": require.resolve("process/browser"),
+    "process": require.resolve("process/browser"),
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert/"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify/browser"),
+    "url": require.resolve("url/"),
+    "buffer": require.resolve("buffer/"),
   };
 
+  // Add plugins
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
-    })
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
   ];
 
+  // Return the modified config
   return config;
 };
