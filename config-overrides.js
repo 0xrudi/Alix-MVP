8fee1882c7dbe6cabcdfc580af3fe36b26050720
rudnickandrew <andrew@rudini.xyz>
2025-01-29 17:14:55 -0700

    Updated webpack config with process polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,28 @@
 const webpack = require('webpack');
 
 module.exports = function override(config) {
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    "vm": require.resolve("vm-browserify"),
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "assert": require.resolve("assert/"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify/browser"),
-    "url": require.resolve("url"),
-    "buffer": require.resolve("buffer/")
+  config.resolve = {
+    ...config.resolve,
+    fallback: {
+      ...config.resolve.fallback,
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "assert": require.resolve("assert/"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "url": require.resolve("url/"),
+      "vm": require.resolve("vm-browserify"),
+      "process": require.resolve("process/browser")
+    }
   };
 
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
-    }),
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
   ];
 
   return config;
