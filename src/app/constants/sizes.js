a9a2158f75ea31041802d88f855d763e40330ffe
rudnickandrew <andrew@rudini.xyz>
2025-01-29 17:02:59 -0700

    Added missing sizes constants file

---
+++ src/app/constants/sizes.js
@@ -0,0 +1,47 @@
+export const cardSizes = {
+    sm: {
+      icon: '2rem',
+      fontSize: 'sm',
+      padding: 1,
+      width: '120px',
+      spacing: 0.5,
+      marginY: 1,
+      columns: {
+        base: 3,
+        sm: 4,
+        md: 6,
+        lg: 8,
+        xl: 10
+      }
+    },
+    md: {
+      icon: '3rem',
+      fontSize: 'md',
+      padding: 1.5,
+      width: '180px',
+      spacing: 0.5,
+      marginY: 1,
+      columns: {
+        base: 2,
+        sm: 3,
+        md: 4,
+        lg: 6,
+        xl: 8
+      }
+    },
+    lg: {
+      icon: '4.5rem',
+      fontSize: 'lg',
+      padding: 2,
+      width: '280px',
+      spacing: 1,
+      marginY: 1.5,
+      columns: {
+        base: 1,
+        sm: 2,
+        md: 3,
+        lg: 4,
+        xl: 5
+      }
+    }
+  };
