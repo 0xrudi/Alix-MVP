2d2b5dfac4fae5020770f141f03f16c60e758787
rudnickandrew <andrew@rudini.xyz>
2025-03-01 17:28:55 -0700

    root router bug for services testing

--- src/RootRouter.js
+++ src/RootRouter.js
@@ -24,12 +24,16 @@ import LibraryPage from './app/components/LibraryPage';
 import ProfilePage from './app/components/ProfilePage';
 import ArtifactDetailPage from './app/components/ArtifactDetailPage/ArtifactDetailPage';
 import ServiceTestComponent from './app/components/ServiceTestComponent';
+import { ServiceProvider } from './services/service-provider';
 
 
 const PrivateRoute = ({ children }) => {
-  const { user, loading } = useAuth();
+  const { user, loading: authLoading } = useAuth();
+  const { loading: serviceLoading } = useServices();
   
-  if (loading) {
+  const isLoading = authLoading || serviceLoading;
+  
+  if (isLoading) {
     return (
       <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
         <Spinner size="xl" />
@@ -46,30 +50,32 @@ const PrivateRoute = ({ children }) => {
 
 const RootRouter = () => {
   return (
-    <Routes>
-      {/* Website Routes */}
-      <Route element={<WebsiteLayout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="privacy" element={<PrivacyPage />} />
-        <Route path="terms" element={<TermsPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="features" element={<FeaturesPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="roadmap" element={<RoadmapPage />} />
-      </Route>
+    <ServiceProvider>
+      <Routes>
+        {/* Website Routes */}
+        <Route element={<WebsiteLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="privacy" element={<PrivacyPage />} />
+          <Route path="terms" element={<TermsPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="features" element={<FeaturesPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="roadmap" element={<RoadmapPage />} />
+        </Route>
 
-      {/* App Routes - No longer protected */}
-      <Route path="app" element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="admin" element={<AdminPage />} />
-        <Route path="library" element={<LibraryPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-        <Route path="artifact" element={<ArtifactDetailPage />} />
-        <Route path="service-test" element={<ServiceTestComponent/>} />
-      </Route>
-    </Routes>
+        {/* App Routes - No longer protected */}
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<PrivateRoute><HomePage /></PrivateRoute>} />
+          <Route path="admin" element={<PrivateRoute><AdminPage /></PrivateRoute>} />
+          <Route path="library" element={<PrivateRoute><LibraryPage /></PrivateRoute>} />
+          <Route path="profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+          <Route path="artifact" element={<PrivateRoute><ArtifactDetailPage /></PrivateRoute>} />
+          <Route path="service-test" element={<PrivateRoute><ServiceTestComponent/></PrivateRoute>} />
+        </Route>
+      </Routes>
+    </ServiceProvider>
   );
 };
 
